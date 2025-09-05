6e571651e2c7d96eef068e2f57c2c30504e07c20
Alessandro <153400275+eualehsan@users.noreply.github.com>
2025-09-05 19:32:00 -0300

    atualização da função calcular

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function clean(){
 
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
+    if(resultado){
+        try {
+            var calc = eval(resultado);
+            if (calc === Infinity || calc === -Infinity) {
+                alert("Não é possível dividir por 0");
+                document.getElementById('resultado').innerHTML = "";
+            } else {
+                document.getElementById('resultado').innerHTML = calc;
+            }
+        } catch (e) {
+            alert("Expressão inválida!");
+            document.getElementById('resultado').innerHTML = "";
+        }
     }
 }
 
 function back(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
 }
